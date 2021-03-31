1b66274e6e453c560c5e20ccfccebde8d37b4cbb
reinaldojunior <reinaldomalinauskasjr@gmail.com>
2021-03-31 19:35:36 -0300

    feat: implementate AudioLoaderResultHandler on PlayerManager

--- src/main/java/dev/rmjr/kingsbot/music/PlayerManager.java
+++ src/main/java/dev/rmjr/kingsbot/music/PlayerManager.java
@@ -1,7 +1,10 @@
 package dev.rmjr.kingsbot.music;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.event.*;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +13,7 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RequiredArgsConstructor
-public class PlayerManager implements AudioEventListener {
+public class PlayerManager implements AudioEventListener, AudioLoadResultHandler {
     private final List<AudioTrack> queue = new CopyOnWriteArrayList<>();
     private final AudioPlayer player;
     private int position = 0;
@@ -27,10 +30,6 @@ public class PlayerManager implements AudioEventListener {
         player.startTrack(queue.get(++position), false);
     }
 
-    public void queueTrack(AudioTrack track) {
-        if(!player.startTrack(track, true)) { queue.add(track); }
-    }
-
     private void onTrackEnd(AudioPlayer player, AudioTrackEndReason reason) {
         if(!reason.mayStartNext) {
             player.stopTrack();
@@ -44,5 +43,33 @@ public class PlayerManager implements AudioEventListener {
         if(event instanceof TrackEndEvent) {
             onTrackEnd(event.player, ((TrackEndEvent) event).endReason);
         }
+    }
+
+    @Override
+    public void trackLoaded(AudioTrack track) {
+        if(!player.startTrack(track, true)) { queue.add(track); }
+    }
+
+    @Override
+    public void playlistLoaded(AudioPlaylist playlist) {
+        if(playlist.getTracks().isEmpty()) { return; }
+
+        AudioTrack track = playlist.getSelectedTrack() == null ?
+                playlist.getTracks().get(0) :
+                playlist.getSelectedTrack();
+
+        if(!player.startTrack(track, true)) { queue.add(track); }
+        playlist.getTracks().remove(track);
+        queue.addAll(playlist.getTracks());
+    }
+
+    @Override
+    public void noMatches() {
+        // TODO
+    }
+
+    @Override
+    public void loadFailed(FriendlyException exception) {
+        // TODO
     }
 }
